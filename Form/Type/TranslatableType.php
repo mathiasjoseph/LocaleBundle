8389e1c4907364ce819e901d8d70933010dba879
miky <mathias89100@gmail.com>
2017-09-18 15:00:12 +0200

    Merge branch 'master' of github.com:mikyprog/Order
    # Conflicts:
    #	.gitignore

--- Form/Type/TranslatableType.php
+++ Form/Type/TranslatableType.php
@@ -12,6 +12,7 @@
 namespace Miky\Bundle\LocaleBundle\Form\Type;
 
 
+use Knp\DoctrineBehaviors\Model\Translatable\Translatable;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -66,19 +67,7 @@ class TranslatableType extends AbstractType
             $options['options']['error_bubbling'] = $options['error_bubbling'];
         }
 
-        switch ($options["required_type"]){
-            case self::REQUIRED_BY_CURRENT_LOCALE :
-                $options["required"] = false;
-                $options[$this->currentLocale . '_options']['required'] = true;
-                break;
-            case self::REQUIRED_BY_DEFAULT_LOCALE:
-                $options["required"] = false;
-                $options[$this->defaultLocale . '_options']["required"] = true;
-                break;
-            case self::ALL_REQUIRED :
-                $options["required"] = true;
-                break;
-        }
+
         foreach ($this->locales as $locale) {
             $builder
                 ->add($options[$locale . '_name'], $options['type'], array_merge($options['options'], $options[$locale . '_options']));
@@ -86,10 +75,17 @@ class TranslatableType extends AbstractType
 
         $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) {
             $form = $event->getForm();
+            /** @var Translatable $parentData */
             $parentData = $event->getForm()->getParent()->getData();
             foreach ($this->locales as $locale) {
-                $value = $this->accessor->getValue($parentData->translate($locale), $form->getPropertyPath());
-                $form->get($locale)->setData($value);
+                $translation = $parentData->translate($locale);
+                $value = $this->accessor->getValue($translation, $form->getPropertyPath());
+                if (!empty($value) && $value != null && $value != "") {
+                    $form->get($locale)->setData($value);
+                }else{
+                    $parentData->removeTranslation($translation);
+                }
+
             }
         });
         $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
@@ -105,19 +101,45 @@ class TranslatableType extends AbstractType
                     $this->accessor->setValue($parentData, $form->getPropertyPath(), $form->get($this->defaultLocale)->getData());
                     break;
             }
+
             foreach ($this->locales as $locale) {
                 $v = $form->get($locale)->getData();
-                if (!empty($v) & $v != null) {
-                    $translation = $parentData->translate($locale);
+                $translation = $parentData->translate($locale);
+                if (!empty($v) && $v != null && $v != "") {
                     $this->accessor->setValue($translation, $form->getPropertyPath(), $v);
+                }else{
+                    $parentData->removeTranslation($translation);
                 }
             }
+            $parentData->mergeNewTranslations();
         });
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         $view->vars['locales'] = $this->locales;
+        switch ($options["required_type"]){
+            case self::REQUIRED_BY_CURRENT_LOCALE :
+                $options["required"] = false;
+                $options[$this->currentLocale . '_options']['required'] = true;
+                break;
+            case self::REQUIRED_BY_DEFAULT_LOCALE:
+                $options["required"] = false;
+                $options[$this->defaultLocale . '_options']["required"] = true;
+                break;
+            case self::ALL_REQUIRED :
+                $options["required"] = true;
+                break;
+        }
+        foreach ($this->locales as $locale) {
+            if (array_key_exists('required', $options[$locale . '_options']) && $options[$locale . '_options']['required'] == true){
+                $view->vars['required_options'][$locale] = $options[$locale . '_options']['required'];
+            }else{
+                $view->vars['required_options'][$locale] = false;
+            }
+
+
+        };
     }
 
     /**
